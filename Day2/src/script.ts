beac71d6db98f8a3174a174093fdc949b692cfc5
huntermahmud <mahmudthedatahunter@gmail.com>
2024-11-06 07:17:24 +0600

    example: 4 day2

--- Day2/src/script.ts
+++ Day2/src/script.ts
@@ -1,4 +1,39 @@
-let playerName = "Muhammad ibn Zain";
+//Example: 1
 
-console.log(playerName);
+// let playerName = "Muhammad ibn Zain";
+
+// console.log(playerName);
+
+// playerName = 26; // error checking while assigning a different types of value
+
+// Example: 2
+
+// let personName;
+
+// personName = "mahmud";
+// personName = 34; // this will not gives error but this is not a good practice
+
+// console.log(personName);
+
+// Example: 3
+
+// function multiply (a:number,b:number){
+//     return a * b;
+// }
+
+// console.log(multiply(3, 5));
+// console.log(multiply("mahmud", 5)); // this will gives error because of function's parameter type definition
+
+
+// Example: 4 array
+
+// let fruits = ['apple', 'banana', 'orange']; // here fruits type is string only
+
+// fruits.push('pineapple'); // no error in this line
+// fruits.push(32) // this line gives error 
+
+// let mixed = ['ahmad', 34, false]; // here type can be string, number, boolean
+// mixed.push('abdullah') // no error generate
+// mixed.push(23) // no error generate
+
 
