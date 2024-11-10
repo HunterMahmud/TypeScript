39209cf6d0add52a852bd930708acf2380ae4147
huntermahmud <mahmudthedatahunter@gmail.com>
2024-11-10 07:32:38 +0600

    Day5 access modifier and shortcut use of modifier in class

--- Day5/dist/script.js
+++ Day5/dist/script.js
@@ -1,33 +1,84 @@
 "use strict";
-// Day 5: video 7
-var userDetails = function (id) {
-    return "hello ".concat(id); // ${user?.name}'s id is ${id} and age is ${user?.age}
-};
-console.log(userDetails(23)); // , {name: 'kashem', age: 34}
-// video: 8 function signature
-// typescript allow user to define a function with his signature or type
-// example: 1
-var userInfo;
-userInfo = userDetails; // not giving error
-console.log(userInfo(23, { name: "string", age: 34 }));
+// // Day 5: video 7
+// // type alias
+// // defining type is sometimes can be bigger in a specific area
+// // we can define type like a variable and we can use it as type
+// // Example: 1
+// type strOrNum = string | number; // type alias
+// type userType = {name: string, age: number}; // type alias
+// const userDetails = (id:strOrNum)=>{ // , user:userType
+//     return `hello ${id}` // ${user?.name}'s id is ${id} and age is ${user?.age}
+// }
+// console.log(userDetails(23)); // , {name: 'kashem', age: 34}
+// // video: 8 function signature
+// // typescript allow user to define a function with his signature or type
+// // example: 1
+// let userInfo: (id: strOrNum, user:userType) => string;
+// userInfo = userDetails // not giving error
+// console.log(userInfo(23, {name:"string", age:34}));
 // video: 9 class
+// class Player{
+//     name: string;
+//     age: number;
+//     country: string;
+//     constructor(n: string, a: number, c: string){
+//         this.name = n;
+//         this.age = a;
+//         this.country = c;
+//     }
+//     play() {
+//         console.log(`${this.name} is ${this.age} years old and he is from ${this.country}`);
+//     }
+// }
+// // making some class using this
+// const tanzim = new Player('Tanzim Hasan Sakib', 24, "Bangladesh");
+// const mrittunjoy = new Player('Mrittunjoy Choudhuri', 25, "Bangladesh");
+// we can use this type as a array in typescirpt
+// for example
+// let players: Player[] = []; // defining this variable as a Player classes array
+// players.push(tanzim);
+// players.push(mrittunjoy);
+// console.log(players);
+// video:10 access modifier
+// default all property is public like: public name: string;
+// class Player{
+//     private name: string; // private i cann't modify or access from outside
+//     public age: number; // i can change and access from outside
+//     readonly country: string;// i can only read but can't modify
+//     constructor(n: string, a: number, c: string){
+//         this.name = n;
+//         this.age = a;
+//         this.country = c;
+//     }
+//     play() {
+//         console.log(`${this.name} is ${this.age} years old and he is from ${this.country}`);
+//     }
+// }
+// // making some class using this
+// const tanzim = new Player('Tanzim Hasan Sakib', 24, "Bangladesh");
+// const mrittunjoy = new Player('Mrittunjoy Choudhuri', 25, "Bangladesh");
+// let players: Player[] = []; // defining this variable as a Player classes array
+// players.push(tanzim);
+// players.push(mrittunjoy);
+// // console.log(tanzim.name); // error for name because of private
+// console.log(tanzim.age); // no error for public
+// console.log(tanzim.country); // no error for country because readonly
+// // tanzim.name = "Rakib" // error for name because of private
+// tanzim.age = 23; // no error for public
+// // tanzim.country = "india" // error because readonly cannt modify
+// console.log(players);
+// shortcut of making class with its modifier dont need to initialize with this.name = n;
 var Player = /** @class */ (function () {
-    function Player(n, a, c) {
-        this.name = n;
-        this.age = a;
-        this.country = c;
+    function Player(name, // private i cann't modify or access from outside
+    age, // i can change and access from outside
+    country // i can only read but can't modify
+    ) {
+        this.name = name;
+        this.age = age;
+        this.country = country;
     }
     Player.prototype.play = function () {
         console.log("".concat(this.name, " is ").concat(this.age, " years old and he is from ").concat(this.country));
     };
     return Player;
 }());
-// making some class using this
-var tanzim = new Player('Tanzim Hasan Sakib', 24, "Bangladesh");
-var mrittunjoy = new Player('Mrittunjoy Choudhuri', 25, "Bangladesh");
-// we can use this type as a array in typescirpt
-// for example
-var players = []; // defining this variable as a Player classes array
-players.push(tanzim);
-players.push(mrittunjoy);
-console.log(players);
--- Day5/src/script.ts
+++ Day5/src/script.ts
@@ -1,62 +1,112 @@
-// Day 5: video 7
+// // Day 5: video 7
 
-// type alias
+// // type alias
 
-// defining type is sometimes can be bigger in a specific area
-// we can define type like a variable and we can use it as type
+// // defining type is sometimes can be bigger in a specific area
+// // we can define type like a variable and we can use it as type
 
-// Example: 1
-type strOrNum = string | number; // type alias
-type userType = {name: string, age: number}; // type alias
+// // Example: 1
+// type strOrNum = string | number; // type alias
+// type userType = {name: string, age: number}; // type alias
 
-const userDetails = (id:strOrNum)=>{ // , user:userType
-    return `hello ${id}` // ${user?.name}'s id is ${id} and age is ${user?.age}
-}
+// const userDetails = (id:strOrNum)=>{ // , user:userType
+//     return `hello ${id}` // ${user?.name}'s id is ${id} and age is ${user?.age}
+// }
 
-console.log(userDetails(23)); // , {name: 'kashem', age: 34}
+// console.log(userDetails(23)); // , {name: 'kashem', age: 34}
 
-// video: 8 function signature
+// // video: 8 function signature
 
-// typescript allow user to define a function with his signature or type
+// // typescript allow user to define a function with his signature or type
 
-// example: 1
-let userInfo: (id: strOrNum, user:userType) => string;
+// // example: 1
+// let userInfo: (id: strOrNum, user:userType) => string;
 
-userInfo = userDetails // not giving error
+// userInfo = userDetails // not giving error
 
-console.log(userInfo(23, {name:"string", age:34}));
+// console.log(userInfo(23, {name:"string", age:34}));
 
 // video: 9 class
 
-class Player{
-    name: string;
-    age: number;
-    country: string;
+// class Player{
+//     name: string;
+//     age: number;
+//     country: string;
 
-    constructor(n: string, a: number, c: string){
-        this.name = n;
-        this.age = a;
-        this.country = c;
-    }
+//     constructor(n: string, a: number, c: string){
+//         this.name = n;
+//         this.age = a;
+//         this.country = c;
+//     }
 
-    play() {
-        console.log(`${this.name} is ${this.age} years old and he is from ${this.country}`);
-    }
-}
+//     play() {
+//         console.log(`${this.name} is ${this.age} years old and he is from ${this.country}`);
+//     }
+// }
 
-// making some class using this
-const tanzim = new Player('Tanzim Hasan Sakib', 24, "Bangladesh");
-const mrittunjoy = new Player('Mrittunjoy Choudhuri', 25, "Bangladesh");
+// // making some class using this
+// const tanzim = new Player('Tanzim Hasan Sakib', 24, "Bangladesh");
+// const mrittunjoy = new Player('Mrittunjoy Choudhuri', 25, "Bangladesh");
 
 // we can use this type as a array in typescirpt
 // for example
 
-let players: Player[] = []; // defining this variable as a Player classes array
+// let players: Player[] = []; // defining this variable as a Player classes array
 
-players.push(tanzim);
-players.push(mrittunjoy);
+// players.push(tanzim);
+// players.push(mrittunjoy);
 
-console.log(players);
+// console.log(players);
 
+// video:10 access modifier
+// default all property is public like: public name: string;
+// class Player{
+//     private name: string; // private i cann't modify or access from outside
+//     public age: number; // i can change and access from outside
+//     readonly country: string;// i can only read but can't modify
 
+//     constructor(n: string, a: number, c: string){
+//         this.name = n;
+//         this.age = a;
+//         this.country = c;
+//     }
 
+//     play() {
+//         console.log(`${this.name} is ${this.age} years old and he is from ${this.country}`);
+//     }
+// }
+
+// // making some class using this
+// const tanzim = new Player('Tanzim Hasan Sakib', 24, "Bangladesh");
+// const mrittunjoy = new Player('Mrittunjoy Choudhuri', 25, "Bangladesh");
+
+// let players: Player[] = []; // defining this variable as a Player classes array
+
+// players.push(tanzim);
+// players.push(mrittunjoy);
+
+// // console.log(tanzim.name); // error for name because of private
+// console.log(tanzim.age); // no error for public
+// console.log(tanzim.country); // no error for country because readonly
+
+// // tanzim.name = "Rakib" // error for name because of private
+// tanzim.age = 23; // no error for public
+// // tanzim.country = "india" // error because readonly cannt modify
+
+// console.log(players);
+
+// shortcut of making class with its modifier dont need to initialize with this.name = n;
+
+class Player {
+  constructor(
+    private name: string, // private i cann't modify or access from outside
+    public age: number, // i can change and access from outside
+    readonly country: string // i can only read but can't modify
+  ) {}
+
+  play() {
+    console.log(
+      `${this.name} is ${this.age} years old and he is from ${this.country}`
+    );
+  }
+}
