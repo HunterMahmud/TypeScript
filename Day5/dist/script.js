3d15dfdc11548238be9af9163bf2b9a71a105d6b
huntermahmud <mahmudthedatahunter@gmail.com>
2024-11-10 07:15:30 +0600

    day5 function signature, class, class type array

--- Day5/dist/script.js
+++ Day5/dist/script.js
@@ -1,6 +1,33 @@
 "use strict";
 // Day 5: video 7
-var userDetails = function (id, user) {
-    return "".concat(user === null || user === void 0 ? void 0 : user.name, "'s id is ").concat(id, " and age is ").concat(user === null || user === void 0 ? void 0 : user.age);
+var userDetails = function (id) {
+    return "hello ".concat(id); // ${user?.name}'s id is ${id} and age is ${user?.age}
 };
-console.log(userDetails(23, { name: 'kashem', age: 34 }));
+console.log(userDetails(23)); // , {name: 'kashem', age: 34}
+// video: 8 function signature
+// typescript allow user to define a function with his signature or type
+// example: 1
+var userInfo;
+userInfo = userDetails; // not giving error
+console.log(userInfo(23, { name: "string", age: 34 }));
+// video: 9 class
+var Player = /** @class */ (function () {
+    function Player(n, a, c) {
+        this.name = n;
+        this.age = a;
+        this.country = c;
+    }
+    Player.prototype.play = function () {
+        console.log("".concat(this.name, " is ").concat(this.age, " years old and he is from ").concat(this.country));
+    };
+    return Player;
+}());
+// making some class using this
+var tanzim = new Player('Tanzim Hasan Sakib', 24, "Bangladesh");
+var mrittunjoy = new Player('Mrittunjoy Choudhuri', 25, "Bangladesh");
+// we can use this type as a array in typescirpt
+// for example
+var players = []; // defining this variable as a Player classes array
+players.push(tanzim);
+players.push(mrittunjoy);
+console.log(players);
--- Day5/src/script.ts
+++ Day5/src/script.ts
@@ -9,8 +9,54 @@
 type strOrNum = string | number; // type alias
 type userType = {name: string, age: number}; // type alias
 
-const userDetails = (id:strOrNum, user:userType)=>{
-    return `${user?.name}'s id is ${id} and age is ${user?.age}`
+const userDetails = (id:strOrNum)=>{ // , user:userType
+    return `hello ${id}` // ${user?.name}'s id is ${id} and age is ${user?.age}
 }
 
-console.log(userDetails(23, {name: 'kashem', age: 34}));
+console.log(userDetails(23)); // , {name: 'kashem', age: 34}
+
+// video: 8 function signature
+
+// typescript allow user to define a function with his signature or type
+
+// example: 1
+let userInfo: (id: strOrNum, user:userType) => string;
+
+userInfo = userDetails // not giving error
+
+console.log(userInfo(23, {name:"string", age:34}));
+
+// video: 9 class
+
+class Player{
+    name: string;
+    age: number;
+    country: string;
+
+    constructor(n: string, a: number, c: string){
+        this.name = n;
+        this.age = a;
+        this.country = c;
+    }
+
+    play() {
+        console.log(`${this.name} is ${this.age} years old and he is from ${this.country}`);
+    }
+}
+
+// making some class using this
+const tanzim = new Player('Tanzim Hasan Sakib', 24, "Bangladesh");
+const mrittunjoy = new Player('Mrittunjoy Choudhuri', 25, "Bangladesh");
+
+// we can use this type as a array in typescirpt
+// for example
+
+let players: Player[] = []; // defining this variable as a Player classes array
+
+players.push(tanzim);
+players.push(mrittunjoy);
+
+console.log(players);
+
+
+
