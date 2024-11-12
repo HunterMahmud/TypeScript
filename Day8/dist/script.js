7a41c8fe9b9030e15f29f6996154553153ff35cd
huntermahmud <mahmudthedatahunter@gmail.com>
2024-11-12 15:44:08 +0600

    day8: rest parameter in function

--- Day8/dist/script.js
+++ Day8/dist/script.js
@@ -1,6 +1,10 @@
 "use strict";
 //* optional parameter in function
-const func = (n, m, l = 20) => {
-    return n * m * l;
+const func = (...rest) => {
+    let sum = 0;
+    for (let i of rest) {
+        sum += i;
+    }
+    return sum;
 };
-console.log(func(2, 3)); // here we can give the argument or not we want
+console.log(func(1, 2, 3, 4, 5, 6, 5, 6));
--- Day8/src/script.ts
+++ Day8/src/script.ts
@@ -10,10 +10,28 @@
 
 
 //* seting default value of optional parameter
-type Func = (n: number, m: number, l?: number) => number;
+// type Func = (n: number, m: number, l?: number) => number;
 
-const func : Func = (n, m, l= 20) => {
-     return n*m*l;
+// const func : Func = (n, m, l= 20) => {
+//      return n*m*l;
+// }
+
+// console.log(func(2,3)); // here we can give the argument or not we want
+
+
+// video: 51:10
+
+// rest parameter in function
+
+type Func = (...rest: number[]) => number;
+
+const func: Func = (...rest) => {
+    let sum: number = 0;
+    
+    for(let i of rest){
+        sum += i;        
+    }
+    return sum;
 }
 
-console.log(func(2,3)); // here we can give the argument or not we want
+console.log(func(1,2,3, 4, 5, 6, 5, 6));
