2e538394b97308cd4065f15e0da69a898b7e6e4a
huntermahmud <mahmudthedatahunter@gmail.com>
2024-11-16 07:36:43 +0600

    Day10: type never and basic class

--- Day10/dist/script.js
+++ Day10/dist/script.js
@@ -1,25 +1,20 @@
+"use strict";
 //video: 1:06:07
 // type never explaining
-
 // const err: Error = new Error(); // this is error type
-
 // // but in below function
 // const errorHandler = () => { 
 //     // return new Error("something went wrong!"); // this one actually "const errorHandler: () => Error"
 //     throw new Error("something went wrong!"); // when directly throw error then "const errorHandler: () => never" // here type never shows
 // }
-
 // classes in typescript
 // normal classes in typescript where all value is public
-class Player  {
-    height; // or you can use like this "height: number = 24;" with direct assign
-    weight;
-    constructor (height: number, weight:number) {
+class Player {
+    constructor(height, weight) {
         this.height = height;
         this.weight = weight;
     }
 }
-
 const jayed = new Player(23, 24);
 console.log(jayed.height);
 console.log(jayed.weight);
