fbd249b5d64fc1e9d14ba171499be19edf702d68
huntermahmud <mahmudthedatahunter@gmail.com>
2024-11-18 16:54:09 +0600

    Day11: private property access using function

--- Day11/dist/script.js
+++ Day11/dist/script.js
@@ -1,22 +1,16 @@
+"use strict";
 // video: 1:08:51
-
 // access modifier in classes
-
-class Player  {
-    private height; 
-    weight;
-    constructor (height: number, weight:number) {
+class Player {
+    constructor(height, weight) {
+        this.getValue = () => {
+            return this.height;
+        };
         this.height = height;
         this.weight = weight;
     }
-    getValue = () => { // this function is allowing the get the value of private property
-        return this.height;
-    }
 }
-
 const jayed = new Player(23, 24);
 // console.log(jayed.height); // we can access private property .. it gives us error
 console.log(jayed.getValue()); // we can access using function call 
-
 console.log(jayed.weight);
-
